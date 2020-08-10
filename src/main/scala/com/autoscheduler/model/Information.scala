fa672f8ffb34e1b5cc502cb21e607840aa7ce733
Isaac Amezcua <iamezcua.dev@gmail.com>
2020-08-10 05:02:09 -0500

    Update Data Access Objects to support persistence

--- src/main/scala/com/autoscheduler/model/Information.scala
+++ src/main/scala/com/autoscheduler/model/Information.scala
@@ -1,17 +1,53 @@
 package com.autoscheduler.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import javax.persistence._
 
 import scala.beans.BeanProperty
 
-case class Information( @BeanProperty @JsonProperty( value = "indicator" ) indicator: Indicator,
-                        @BeanProperty @JsonProperty( value = "country" ) country: Country,
-                        @BeanProperty @JsonProperty( value = "countryiso3code" ) countryiso3code: String,
-                        @BeanProperty @JsonProperty( value = "date" ) date: String,
-                        @BeanProperty @JsonProperty( value = "value" ) value: String,
-                        @BeanProperty @JsonProperty( value = "unit" ) unit: String,
-                        @BeanProperty @JsonProperty( value = "obs_status" ) obs_status: String,
-                        @BeanProperty @JsonProperty( value = "decimal" ) decimal: String ) {
-  def this( ) = this( Indicator( "", "" ), Country( "", "" ), "", "", "", "", "", "" )
+@Entity
+class Information( ) {
+  @Id
+  @GeneratedValue( strategy = GenerationType.AUTO )
+  @Column( name = "id", unique = true, nullable = false )
+  @BeanProperty
+  var id: Integer = 0
+  
+  @BeanProperty
+  @JsonProperty( value = "indicator" )
+  @Embedded
+  var indicator: Indicator = Indicator( "", "" )
+  
+  @BeanProperty
+  @JsonProperty( value = "country" )
+  @Embedded
+  var country: Country = Country( "", "" )
+  
+  @BeanProperty
+  @JsonProperty( value = "countryiso3code" )
+  var countryiso3code: String = ""
+  
+  @BeanProperty
+  @JsonProperty( value = "date" )
+  var date: String = ""
+  
+  @BeanProperty
+  @JsonProperty( value = "value" )
+  var value: String = ""
+  
+  @BeanProperty
+  @JsonProperty( value = "unit" )
+  var unit: String = ""
+  
+  @BeanProperty
+  @JsonProperty( value = "obs_status" )
+  var obs_status: String = ""
+  
+  @BeanProperty
+  @JsonProperty( value = "decimal" )
+  var decimal: String = ""
+  
+  
+  override def toString = s"Information($id, $indicator, $country, $countryiso3code, $date, $value, $unit, $obs_status, $decimal)"
 }
 
