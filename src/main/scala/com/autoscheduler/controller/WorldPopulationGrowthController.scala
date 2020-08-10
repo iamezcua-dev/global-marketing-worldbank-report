af5b55c70f28812a71b8acba7215622f7bf838f0
Isaac Amezcua <iamezcua.dev@gmail.com>
2020-08-10 05:04:48 -0500

    Provide persistence mechanism using local SQLite database

--- src/main/scala/com/autoscheduler/controller/WorldPopulationGrowthController.scala
+++ src/main/scala/com/autoscheduler/controller/WorldPopulationGrowthController.scala
@@ -3,24 +3,33 @@ package com.autoscheduler.controller
 import java.util.Locale
 import java.util.Locale.IsoCountryCode
 
-import com.autoscheduler.model.Information
 import com.autoscheduler.service.WorldPopulationGrowthService
 import com.typesafe.scalalogging.LazyLogging
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation._
 
 @RestController
-class WorldPopulationGrowthController @Autowired()( service: WorldPopulationGrowthService ) extends LazyLogging {
+class WorldPopulationGrowthController( ) extends LazyLogging {
+  
+  @Autowired()
+  var service: WorldPopulationGrowthService = _
+  
+  @RequestMapping( value = Array( "/results" ), method = Array( RequestMethod.GET ) )
+  def retrieveData( ): Unit = {
+    service.getInformation
+  }
   
   @RequestMapping( value = Array( "/indicators" ), method = Array( RequestMethod.GET ) )
-  def getWorldsPopulationGrowth( ): List[ Information ] = {
-    val countries = Locale.getISOCountries( IsoCountryCode.PART1_ALPHA3 ).toArray.map( _.toString )
-    val countryData = countries.map( country => {
-      val countryStatisticsList = service.getWorldPopulationGrowth( country )
-      logger.info( s"$countryStatisticsList" )
-      countryStatisticsList
-    } )
-    countryData.head
+  def getAndStoreWorldsPopulationGrowth( ): Unit = {
+    Locale.getISOCountries( IsoCountryCode.PART1_ALPHA3 )
+        .toArray.map( _.toString )
+        .map( country => {
+          val countryStatisticsList = service.getWorldPopulationGrowth( country )
+          logger.info( s"$countryStatisticsList" )
+          countryStatisticsList
+        } )
+        .foreach( countryStatistics =>
+          service.saveStatisticsToDatabase( countryStatistics ) )
   }
   
   @RequestMapping( value = Array( "/sample" ), method = Array( RequestMethod.GET ) )
